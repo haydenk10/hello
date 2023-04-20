4b65ad3f4f4ab2d88c4b7499e94341c2c2e226d0
haydenk10 <70301221+haydenk10@users.noreply.github.com>
2023-04-20 09:01:13 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 type Server struct{}
 
-func (s *S**erver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.Status*******OK)
-   w.Header().Set("C****ontent-Type", "application/json")
-   w.Wri*****te([]byte(`{"message": "Hello Golden Rams SP2023"}`))
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   w.WriteHeader(http.StatusOK)
+   w.Header().Set("Content-Type", "application/json")
+   w.Write([]byte(`{"message": "Hello Golden Rams SP2023"}`))
 }
 
 func main() {
   s := &Server{}
-  http.Han***dle("/", s)
-  log.Fatal(htt*****p.ListenAndServe(":8080", nil))
+  http.Handle("/", s)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
